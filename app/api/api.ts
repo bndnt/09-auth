eca08d4381803a32bb629ab450907ed051049c9f
bndnt <67003594+bndnt@users.noreply.github.com>
2025-12-30 03:38:45 -0500

    mentor 2.0 proxy.ts

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,14 +1,6 @@
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://notehub-api.goit.study",
   withCredentials: true,
 });
-
-export type ApiError = AxiosError<{
-  error: string;
-  response: {
-    message: string;
-    validation: { body: { message: string } };
-  };
-}>;
